e9a6198e8a35d2806f2fd1fbe85a283c39212fc9
Akash R <akashrawatcats@gmail.com>
2025-07-05 13:26:57 +0530

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Blog
+            Blog!
           </a>
         </div>
       </main>
