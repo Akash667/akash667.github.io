10daf4b6d24e746c12d9710e4f4306891f8be561
Akash R <akashrawatcats@gmail.com>
2025-07-07 01:00:25 +0530

    fix css for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,17 +3,18 @@ import styles from "./page.module.css";
 export default function BlogIndex() {
   // Replace with your blog post data
   const posts = [
-    { slug: "first-post", title: "First Post" },
-    { slug: "second-post", title: "Second Post" },
+    { slug: "first-post", title: "First Post" , content:"Wow there is so much going on here i watned to put some stuff on here"},
+    { slug: "second-post", title: "Second Post", content: "wow i need to hmm perhaps thinkm ore about htis and put this up" },
   ];
 
   return (
     <main className={styles.main}>
-      <h2>All Posts</h2>
+    <h2>All Posts</h2> 
       <ul className={styles.postList}>
         {posts.map(post => (
           <li key={post.slug}>
-            <a href={`/blog/${post.slug}`}>{post.title}</a>
+            <a  className={styles.listTitle} href={`/blog/${post.slug}`}>{post.title}</a>
+            <div>{post.content}</div>
           </li>
         ))}
       </ul>
